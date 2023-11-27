1aefc6de38ee723926c24452c334d12ba2b1cbcc
gentwocoder <adetoyese0511@gmail.com>
2023-11-27 20:32:03 +0100

    My Video app

--- videochat/videochat/settings.py
+++ videochat/videochat/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
